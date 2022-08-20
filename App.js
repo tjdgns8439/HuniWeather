996053ce1729bd6357c23db71c1fd86aea3f5458
tjdgns8439 <tjdgns8439@inha.edu>
2022-08-20 17:27:37 +0900

    flex
    레이아웃을 사용하여 비율에 맞게 화면 색깔 조정

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Helloooooooo!!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{flex: 1, backgroundColor: "tomato"}}></View>
+      <View style={{flex: 1, backgroundColor: "teal"}}></View>
+      <View style={{flex: 1, backgroundColor: "orange"}}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
